9420d14c75038bb175ec9776d79be2b5c79daa1f
Dee <dweinacht@workmail.com>
2023-02-08 13:26:42 -0400

    Format input data, display on page

--- backend.py
+++ backend.py
@@ -1,32 +1,32 @@
 import requests
-API_KEY = '141710af2113bab9f55ef73e1bcd33d5'
+
+API_KEY = 'a6e5c24f4c3a18f0e44dfae3c9a54de8'
 
 
-def get_forecast(location, forecast_days=1, forecast_type=None,
-                 scale='Celsius'):
+def get_forecast(location, days=1, scale='Celsius'):
+    """
+    This method uses the openweathermap API to retrieve a dictionary of
+    forecast data at the given location over the specified length of time.
+    :param location: str
+    :param days: int
+    :param scale: str
+    :return: dict
+    """
     if scale == 'Celsius':
         units = 'metric'
     elif scale == 'Fahrenheit':
         units = 'imperial'
     else:
         pass
+    num_records = 8 * days  # every 3 hours, 8 records per day
+
     url = 'https://api.openweathermap.org/data/2.5/forecast?' \
           f'q={location}&' \
           f'appid={API_KEY}&' \
+          f'cnt={num_records}&' \
           f'units={units}'
     response = requests.get(url)
-    data = response.json()
-    num_records = 8*forecast_days  # every 3 hours, 8 records per day
-    data = data["list"][:num_records]
-
-    match forecast_type:
-        case 'Temperature':
-            forecast_data = [dic['main']['temp'] for dic in data]
-        case 'Conditions':
-            forecast_data = [dic['weather'][0]['main'] for dic in data]
+    forecast_data = response.json()
+    forecast_data = forecast_data['list']
 
     return forecast_data
-
-
-if __name__=='__main__':
-    print(get_forecast(location='Tokyo', forecast_days=3, forecast_type='Temperature'))
--- main.py
+++ main.py
@@ -1,27 +1,60 @@
+"""
+
+"""
+
 import streamlit as st
 import plotly.express as px
-from backend import *
+from backend import get_forecast
+import datetime
 
+
+# Set up page, add widgets and labels
 st.set_page_config(page_title='Weather Forecaster')
 st.title('Upcoming Weather')
-location = st.text_input('Location: ')
+location = st.text_input('Location: ', help='Enter a city name')
+temp_scale = st.radio('Temperature Scale', ('Celsius', 'Fahrenheit'),
+                      key='scale', horizontal=True)
 forecast_days = st.slider('Days to display:', min_value=1, max_value=5,
                           help='Slide to select the number of '
                                'days to forecast')
 forecast_type = st.selectbox('Select forecast type: ',
                              ('Temperature', 'Conditions'))
-if forecast_type == 'Temperature':
-    temp_scale = st.radio('Temperature Scale', ('Celsius', 'Fahrenheit'),
-             key='scale', horizontal=True)
-st.subheader(f'{location} {forecast_type} for the next {forecast_days} days')
+st.header(f'{location} {forecast_type} for the next {forecast_days} days')
 
 
-def get_forecast(days):
-    dates = ['2023-1-2', '2023-1-3', '2023-1-4']
-    temperatures = [10, 20, 30]
-    return dates, temperatures
+if location:
+    try:
+        forecast_data = get_forecast(location,
+                                     days=forecast_days, scale=temp_scale)
+        dates = [datetime.datetime.fromisoformat(dic['dt_txt'])
+                 for dic in forecast_data]
+        match forecast_type:
+            case 'Temperature':
+                temperature_data = [dic['main']['temp']
+                                    for dic in forecast_data]
+                fig = px.line(x=dates, y=temperature_data, markers=True,
+                              labels={'x': 'Date',
+                                      'y': f'Temperature ({temp_scale})'})
+                st.plotly_chart(fig)
 
-d, t = get_forecast(forecast_days)
-fig = px.line(x=d, y=t,
-              labels={'x': 'Date', 'y': 'Temperature (C)'})
-st.plotly_chart(fig)
+            case 'Conditions':
+                images_dict = {'Clear': 'images/clear.png',
+                               'Clouds': 'images/cloudy.png',
+                               'Rain': 'images/rain.png',
+                               'Snow': 'images/snow.png'}
+                conditions_data = \
+                    [dic['weather'][0]['main'] for dic in forecast_data]
+                conditions_with_dates = zip(dates, conditions_data)
+
+                for condition in conditions_with_dates:
+                    col1, col2 = st.columns(2)
+                    with col1:
+                        st.subheader(condition[0].strftime('%A %B %d %y'))
+                        st.write(condition[0].strftime('%I:%M %p'))
+                        st.write(condition[1])
+                    with col2:
+                        st.write('')
+                        st.image(images_dict[condition[1]], width=200)
+
+    except KeyError:
+        st.write('Please enter a valid city name!')
